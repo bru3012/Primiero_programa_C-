3b2d79179fdc6e443735e132aebc8866d7f3f864
bru3012 <168659849+bru3012@users.noreply.github.com>
2024-08-30 14:33:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace PrimeiroPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+        }
+    }
+}
